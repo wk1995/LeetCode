0a4b86d8e32c99cff34180193c0498ecb1e97da3
wk1995 <1226426603@qq.com>
2018-07-17 07:47:47 +0800

    添加一些曾经写过的题目

--- src/com/wk/Main.java
+++ src/com/wk/Main.java
@@ -2,6 +2,7 @@ package com.wk;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
@@ -42,5 +43,101 @@ public class Main {
 
     }
 
+    private static List<Integer> q728(){
+        int left=1;
+        int right=22;
+        List<Integer> list=new ArrayList();
+        for(int i=left;i<=right;i++){
+            if(way(i))
+                list.add(i);
+        }
+        ergodicList(list);
+        return list;
+    }
+    private static boolean way(int j){
+        int i=j;
+        int a;
+        while(i!=0){
+            a=i%10;
+
+            if(a==0 || j%a!=0)
+                return false;
+            i/=10;
+        }
+        return true;
+    }
+
+
+
+
+    private static int[][] q832(){
+        int[][] A={{1,1,0},{1,0,1},{0,0,0}};
+        for(int i=0;i<A.length;i++){
+            swapArray(A[i]);
+        }
+
+        for(int i=0;i<A.length;i++) {
+            for (int j = 0; j < A[i].length; j++)
+                System.out.print(A[i][j] + ",");
+            System.out.println();
+        }
+
+        for(int i=0;i<A.length;i++)
+            for(int j=0;j<A[i].length;j++)
+                A[i][j]=A[i][j]^1;
+        for(int i=0;i<A.length;i++) {
+            for (int j = 0; j < A[i].length; j++)
+                System.out.print(A[i][j] + ",");
+            System.out.println();
+        }
+        return A;
+    }
+    private static void swapArray(int[] a){
+        for(int j=0;j<=a.length/2-1;j++){
+            a[j]=a[j]^a[a.length-1-j];
+            a[a.length-1-j]=a[j]^a[a.length-1-j];
+            a[j]=a[j]^a[a.length-1-j];
+        }
+
+    }
+
+
+    private static int  q852(){
+        int[] A={0,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1,0};
+        for(int i=1;i<A.length-1;i++){
+            if(isMaxInLeft(A,i) && isMaxInRight(A,i))
+                return i;
+        }
+        return -1;
+    }
+    private static boolean isMaxInLeft(int[] a,int i){
+        while (i>0){
+            if(a[i]<=a[i-1])
+                return false;
+            i--;
+        }
+        return true;
+    }
+    private static boolean isMaxInRight(int[] a,int i){
+        while (i<a.length-1){
+            if(a[i]<=a[i+1])
+                return false;
+            i++;
+        }
+        return true;
+    }
+
+
+    private static void ergodicList(List list){
+        if(list.isEmpty()){
+            System.out.println("无数据");
+        }
+        Iterator iterable=list.iterator();
+        while (iterable.hasNext())
+        {
+            System.out.println(iterable.next());
+        }
+
+    }
 
 }
