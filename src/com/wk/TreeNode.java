227d859a8d2f7ce7a0db9cfb6575d73accba5c25
wk1995 <1226426603@qq.com>
2018-08-20 23:30:21 +0800

    添加数据结构->树

---
+++ src/com/wk/TreeNode.java
@@ -0,0 +1,21 @@
+package com.wk;
+
+/**
+ * <pre>
+ *      author : wk
+ *      e-mail : 1226426603@qq.com
+ *      time   : 2018/8/20/23:20
+ *      desc   : 数据结构->树
+ *      GitHub : https://github.com/wk1995
+ *      CSDN   : http://blog.csdn.net/qq_33882671
+ * </pre>
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
