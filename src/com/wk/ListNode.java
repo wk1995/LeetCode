3234263c06ed184c9f2e2222413377ba95b08a8e
wk1995 <1226426603@qq.com>
2018-08-20 23:25:46 +0800

    添加数据结构->单链表

---
+++ src/com/wk/ListNode.java
@@ -0,0 +1,19 @@
+package com.wk;
+
+/**
+ * <pre>
+ *      author : wk
+ *      e-mail : 1226426603@qq.com
+ *      time   : 2018/08/2018/8/12
+ *      desc   : 单链表数据结构
+ *      GitHub : https://github.com/wk1995
+ *      CSDN   : http://blog.csdn.net/qq_33882671
+ * </pre>
+ */
+public class ListNode {
+    int val;
+    public ListNode next;
+    public ListNode(int x){
+        val=x;
+    }
+}
