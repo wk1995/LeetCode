e46fe1004fe64a8885a69b01ed44260ead8301b3
wk1995 <1226426603@qq.com>
2018-09-13 08:03:22 +0800

    优化树数据结构，属性与方法对外

--- src/com/wk/data/structure/TreeNode.java
+++ src/com/wk/data/structure/TreeNode.java
@@ -11,11 +11,11 @@ package com.wk.data.structure;
  * </pre>
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-    TreeNode(int x) {
+    public TreeNode(int x) {
         val = x;
     }
 }
