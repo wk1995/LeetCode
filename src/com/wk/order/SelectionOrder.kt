a2e4f24b01340f412e367aad22dda662fed8da96
wk1995 <1226426603@qq.com>
2018-12-08 15:26:48 +0800

    增加排序之选择排序

---
+++ src/com/wk/order/SelectionOrder.kt
@@ -0,0 +1,36 @@
+package com.wk.order
+
+/**
+ * <pre>
+ *      author : wk
+ *      e-mail : 1226426603@qq.com
+ *      time   : 2018/11/26/22:59
+ *      desc   : 选择排序
+ *      GitHub : https://github.com/wk1995
+ *      CSDN   : http://blog.csdn.net/qq_33882671
+ * </pre>
+ */
+class SelectionOrder(val sortList: ArrayList<Int>) : Order() {
+
+
+    override fun descOrder(sortList: ArrayList<Int>): ArrayList<Int> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun ascOrder(sortList: ArrayList<Int>): ArrayList<Int> {
+        val sorts = ArrayList<Int>()
+        for (i in sortList.indices) {
+            var max = sortList[0]
+            var tmp = 0
+            for (j in sortList.indices) {
+                if (sortList[j] >= max) {
+                    tmp = j
+                    max = sortList[j]
+                }
+            }
+            sorts.add(sortList[tmp])
+            sortList.remove(sortList[tmp])
+        }
+        return sorts
+    }
+}
