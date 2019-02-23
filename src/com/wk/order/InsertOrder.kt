1bc81d0d4c945d396b7fd39f8cb3a69ccca2bb1d
wk1995 <1226426603@qq.com>
2019-02-23 17:44:51 +0800

    增加排序之插入排序

---
+++ src/com/wk/order/InsertOrder.kt
@@ -0,0 +1,33 @@
+package com.wk.order
+
+/**
+ * <pre>
+ *      author : wk
+ *      e-mail : 1226426603@qq.com
+ *      time   : 2019/1/27/21:26
+ *      desc   : 插入排序
+ *      GitHub : https://github.com/wk1995
+ *      CSDN   : http://blog.csdn.net/qq_33882671
+ * </pre>
+ */
+class  InsertOrder<T> : Order() {
+    override fun <T : Comparable<Any>> descOrder(sortList: ArrayList<T>): ArrayList<T> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <T : Comparable<Any>> ascOrder(sortList: ArrayList<T>): ArrayList<T> {
+        val size = sortList.size
+        //正在排序的T
+//        var current: T
+        for (i in 0 until size) {
+            var preIndex:Int = i - 1
+            val current = sortList[i]
+           while(preIndex>=0 && current<sortList[preIndex]){
+               sortList[i]=sortList[preIndex]
+               preIndex--
+           }
+            sortList[preIndex+1]=current
+        }
+        return sortList
+    }
+}
