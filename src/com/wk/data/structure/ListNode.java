f01ef0c89242cf91da80706764d99644608659c5
wk1995 <1226426603@qq.com>
2019-08-14 21:23:15 +0800

    增加单链表数据结构构造方式以及toString的重写

--- src/com/wk/data/structure/ListNode.java
+++ src/com/wk/data/structure/ListNode.java
@@ -1,5 +1,8 @@
 package com.wk.data.structure;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * <pre>
  *      author : wk
@@ -15,5 +18,47 @@ public class ListNode {
     public ListNode next;
     public ListNode(int x){
         val=x;
+    }
+
+    public static ListNode createListNode( Integer ...xs){
+        return createListNode(Arrays.asList(xs));
+    }
+
+    public static ListNode createListNode( List<Integer> vals){
+        if(vals==null || vals.size()<=0){
+            return null;
+        }
+        ListNode result=null;
+        ListNode pre=null;
+      /*  vals.forEach(new Consumer<Integer>(){
+            @Override
+            public Consumer<Integer> andThen(Consumer<? super Integer> after) {
+                return null;
+            }
+
+            @Override
+            public void accept(Integer integer) {
+
+            }
+        });*/
+        for(int i=0;i<vals.size();i++){
+            if(result==null) {
+                result =new  ListNode(vals.get(i));
+                pre=result;
+            }else{
+                ListNode next=new  ListNode(vals.get(i));
+                pre.next=next;
+                pre=next;
+            }
+        }
+        return result;
+    }
+    @Override
+    public String toString() {
+        if (next != null) {
+            return val + "," + next.toString();
+        } else {
+            return String.valueOf(val);
+        }
     }
 }
