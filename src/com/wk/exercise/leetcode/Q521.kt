37c9478d7a2a84361931f8902b1b6d97abc863e9
wk1995 <1226426603@qq.com>
2019-09-23 17:09:14 +0800

    提交Q521( 最长特殊序列 Ⅰ)答案

--- src/com/wk/exercise/leetcode/Q521.kt
+++ src/com/wk/exercise/leetcode/Q521.kt
@@ -11,11 +11,13 @@ package com.wk.exercise.leetcode
  *      CSDN   : http://blog.csdn.net/qq_33882671 <br/>
  *      desc   : 最长特殊序列 Ⅰ
 
-给定两个字符串，你需要从这两个字符串中找出最长的特殊序列。最长特殊序列定义如下：该序列为某字符串独有的最长子序列（即不能是其他字符串的子序列）。
+给定两个字符串，你需要从这两个字符串中找出最长的特殊序列。最长特殊序列定义如下：
+    该序列为某字符串独有的最长子序列（即不能是其他字符串的子序列）。
 
-子序列可以通过删去字符串中的某些字符实现，但不能改变剩余字符的相对顺序。空序列为所有字符串的子序列，任何字符串为其自身的子序列。
+    子序列可以通过删去字符串中的某些字符实现，但不能改变剩余字符的相对顺序。
+    空序列为所有字符串的子序列，任何字符串为其自身的子序列。
 
-输入为两个字符串，输出最长特殊序列的长度。如果不存在，则返回 -1。
+    输入为两个字符串，输出最长特殊序列的长度。如果不存在，则返回 -1。
 
 示例 :
 
@@ -38,7 +40,11 @@ class Q521:Q {
     override fun answer() {
 
     }
-    fun findLUSlength(a: String, b: String): Int {
-        return 0;
+    private fun findLUSlength(a: String, b: String): Int {
+        return if(a==b){
+            -1
+        }else{
+            a.length.coerceAtLeast(b.length)
+        }
     }
 }
