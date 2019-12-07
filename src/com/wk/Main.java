f59666d067fbb1e82045215139b9ee6993e8fea9
wk1995 <1226426603@qq.com>
2019-12-07 15:05:10 +0800

    优化main代码

--- src/com/wk/Main.java
+++ src/com/wk/Main.java
@@ -1,22 +1,20 @@
 package com.wk;
 
-import com.wk.data.structure.TwoForkTree;
 import com.wk.exercise.ExerciseType;
 import com.wk.order.BubbleSort;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import static com.wk.ErgodicType.STRATUM_LEFT_TO_RIGHT;
 import static com.wk.exercise.ExerciseType.LEET_CODE;
 
+/**
+ * @author wangkang
+ */
+@SuppressWarnings(SuppressConstant.UNUSED)
 public class Main {
-
-
-
     public static void main(String[] args) {
         System.out.println("请输入题目序号： ");
         Scanner scanner=new Scanner(System.in);
@@ -42,23 +40,17 @@ public class Main {
     private static void solve(int i,ExerciseType type){
         String className="com.wk.exercise."+type.getName()+".Q"+i;
         try {
-            Class c = Class.forName(className);
-            Method m=c.getMethod("answer",new  Class[ 0 ]);
+            Class<?> c = Class.forName(className);
+            Method m=c.getMethod("answer");
             Object o=c.newInstance();
-            m.invoke(o, new  Object[]{});
+            m.invoke(o);
         }catch (ClassNotFoundException e){
             System.out.println("没有"+type.getName()+" Q"+i+"的答案");
 
-        }catch (IllegalAccessException e){
-            e.printStackTrace();
-        }catch (NoSuchMethodException e){
+        } catch (NoSuchMethodException e){
             System.out.println(type.getName()+i+" 没有answer方法");
             e.printStackTrace();
-        }catch (InstantiationException e){
-            e.printStackTrace();
-        }catch (InvocationTargetException e){
-            e.printStackTrace();
-        }catch (Exception e){
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
