2db62d5748ced14843bdc5283728e10e615847b3
wk1995 <1226426603@qq.com>
2020-02-26 22:17:57 +0800

    提交Q1047(删除字符串中的所有相邻重复项)答案 执行用时 :605 ms, 在所有 Java 提交中击败了12.66%的用户 内存消耗 :42.5 MB 在所有 Java 提交中击败了5.95%的用户

--- src/com/wk/exercise/leetcode/Q1047.java
+++ src/com/wk/exercise/leetcode/Q1047.java
@@ -1,5 +1,7 @@
 package com.wk.exercise.leetcode;
 
+import com.wk.SuppressConstant;
+
 /**
  * <pre>
  *      @author : wk <br/>
@@ -9,37 +11,51 @@ package com.wk.exercise.leetcode;
  *      address:
  *      CSDN   : http://blog.csdn.net/qq_33882671 <br/>
  *      desc   : 删除字符串中的所有相邻重复项
-
- 给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
-
- 在 S 上反复执行重复项删除操作，直到无法继续删除。
-
- 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
-
-  
-
- 示例：
-
- 输入："abbaca"
- 输出："ca"
- 解释：
- 例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
-  
-
- 提示：
-
- 1 <= S.length <= 20000
- S 仅由小写英文字母组成。
-
- 来源：力扣（LeetCode）
- 链接：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string
- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-
+ *
+ * 给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+ *
+ * 在 S 上反复执行重复项删除操作，直到无法继续删除。
+ *
+ * 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入："abbaca"
+ * 输出："ca"
+ * 解释：
+ * 例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= S.length <= 20000
+ * S 仅由小写英文字母组成。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * </pre>
  */
-//todo
-public class Q1047 {
-    public String removeDuplicates(String s) {
-        return null;
+@SuppressWarnings(SuppressConstant.UNUSED)
+public class Q1047 implements Q {
+    @Override
+    public void answer() {
+        System.out.println(removeDuplicates("abbaca"));
+    }
+
+    private String removeDuplicates(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                s = s.substring(0, i) + s.substring(i + 2, s.length());
+                return removeDuplicates(s);
+            }
+        }
+        return s;
     }
 }
