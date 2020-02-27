1ce39ff421ca23a7c3d4c83e0f25041e7c745fa4
wk1995 <1226426603@qq.com>
2020-02-27 20:23:03 +0800

    提交Q107(二叉树的层次遍历 II)答案

--- src/com/wk/exercise/leetcode/Q107.java
+++ src/com/wk/exercise/leetcode/Q107.java
@@ -1,7 +1,9 @@
 package com.wk.exercise.leetcode;
 
+import com.wk.SuppressConstant;
 import com.wk.data.structure.TwoForkTree;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -38,13 +40,36 @@ import java.util.List;
 
  * </pre>
  */
-//TODO
+@SuppressWarnings(SuppressConstant.UNUSED)
 public class Q107 implements Q {
     @Override
     public void answer() {
-
+        TwoForkTree root=new TwoForkTree(3,9,20);
+        root.right=new TwoForkTree(20,15,7);
     }
-    public List<List<Integer>> levelOrderBottom(TwoForkTree root) {
-        return null;
+    private List<List<Integer>> levelOrderBottom(TwoForkTree root) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        LinkedList<TwoForkTree> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            LinkedList<Integer> levels = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TwoForkTree node = queue.poll();
+                levels.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            //区别点：头插入
+            result.addFirst(levels);
+        }
+        return result;
     }
 }
