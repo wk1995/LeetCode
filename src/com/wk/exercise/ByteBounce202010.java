fe2876e6ff561888ac2796b9d652bac0ed48a28f
wk1995 <1226426603@qq.com>
2020-10-29 20:37:39 +0800

    属于字节跳动的面试题

---
+++ src/com/wk/exercise/ByteBounce202010.java
@@ -0,0 +1,15 @@
+package com.wk.exercise;
+
+/**
+ * <pre>
+ *      @author : wk <br/>
+ *      e-mail : 1226426603@qq.com <br/>
+ *      time   : 2020/10/11 <br/>
+ *      desc   : 字节跳动算法题        <br/>
+ *
+ *      address:
+ *      GitHub : https://github.com/wk1995 <br/>
+ *      CSDN   : http://blog.csdn.net/qq_33882671 <br/>
+ * </pre>
+ */
+public interface ByteBounce202010{}
